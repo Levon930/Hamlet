70fbcbf31c12155caebaf427fb8dc7c0338eb249
Levon <61090194+Levon930@users.noreply.github.com>
2021-07-09 00:34:34 +0400

    hsbhss

--- src/components/App/Pages/Accelerator.js
+++ src/components/App/Pages/Accelerator.js
@@ -1,9 +1,18 @@
 import React from "react";
+import shutov from "../../../static/img/Բիզնեսը դու ես-1.svg";
 
 const Accelerator = () => {
   return (
-    <div>
-      <h1>Coming Soon!</h1>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      <img src={shutov} style={{ width: "90%", padding: "100px" }} />
     </div>
   );
 };
--- src/components/App/Pages/Conference.js
+++ src/components/App/Pages/Conference.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import shutov from "../../../static/img/Բիզնեսը դու ես-1.svg";
 const Conference = () => {
   return (
-    <div>
-      <h1>Coming Soon!</h1>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      <img src={shutov} style={{ width: "90%", padding: "100px" }} />
     </div>
   );
 };
